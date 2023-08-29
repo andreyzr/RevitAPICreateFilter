28ea04a39e6a442e849fd938328a8cdfbe55f964
andreyzr43 <andreyzr43@yandex.ru>
2023-08-29 07:53:35 +0300

    1

---
+++ RevitAPIHW7.1/MainViewViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPIHW7._1.Wrappers;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevitAPIHW7._1
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+        private Document _doc;
+
+        public List<FamilySymbol> Sheets { get; }
+        public ExportSheetType SelectedType { get; set; }
+
+        public DelegateCommand AddFilterCommand { get; }
+        public string ParameterName { get; set; }
+        public string ParameterValue { get; set; }
+
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            _doc = _commandData.Application.ActiveUIDocument.Document;
+            Sheets = Utils.GetSheets(_commandData);
+            AddFilterCommand = new DelegateCommand(OnAddFilterCommand);
+        }
+
+        private void OnAddFilterCommand()
+        {
+
+
+            using (var ts = new Transaction(_doc, "Add filter to schedule"))
+            {
+                ts.Start();
+
+                ts.Commit();
+            }
+        }
+
+
+
+        public event EventHandler CloseRequest;
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);//закрытие окна
+        }
+
+
+
+    }
+}
---
+++ RevitAPIHW7.1/Utils.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIHW7._1
+{
+    public class Utils
+    {
+        public static List<FamilySymbol> GetSheets(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilySymbol> sheetsTypes = new FilteredElementCollector(doc)
+                                                        .OfClass(typeof(FamilySymbol))
+                                                        .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                                                        .Cast<FamilySymbol>()
+                                                        .ToList();
+
+            return sheetsTypes;
+        }
+    }
+}
